a0d8b5fb68d56cfeff1fb9473da85edf0bb573b2
Jeremy Castagno <jeremybyu@gmail.com>
2016-09-10 14:03:47 -0400

    Dot Prodcut Complete

--- c/matmult.c
+++ c/matmult.c
@@ -2,18 +2,68 @@
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/time.h>
+#include <inttypes.h>
+
 
 // Note the passing of a function reference as the last argument to bisect()
 
 void read_dimension(int *row, int *col, char matrix_name);
 int read_file(char *fileName, double** mat, int row, int col);
 double** create_array(int row, int col);
+void print_matrix(char* fileName, double** mat, int row, int col);
+double mult_row_col(double** mat1, double** mat2, int row, int col, int size);
+double** dot_product(double** mat1, double** mat2, int dim1_row, int dim1_col, int dim2_row, int dim2_col);
+int dim_compatible(int mat1_col, int mat2_row);
+
+int64_t utime_now (void){
+	struct timeval tv;
+	gettimeofday (&tv, NULL);
+	return (int64_t) tv.tv_sec * 1000000 + tv.tv_usec;
+}
+int dim_compatible(int mat1_col, int mat2_row) {
+  return mat1_col == mat2_row;
+}
+
+double** dot_product(double** mat1, double** mat2, int dim1_row, int dim1_col, int dim2_row, int dim2_col) {
+  int i, j;
+  double** product = create_array(dim1_row, dim2_col);
+  for(i = 0; i < dim1_row; i++) {
+    for(j = 0; j <  dim2_col; j++) {
+      double val = mult_row_col(mat1, mat2, i, j, dim1_col);
+      product[i][j] = val;
+      // printf("%f", val);
+    }
+  }
+  return product;
+}
+
+void print_matrix(char* fileName, double** mat, int row, int col) {
+  int i, j;
+  for(i = 0; i < row; i++) {
+    for(j = 0; j < col; j++) {
+      printf("%f, ", mat[i][j]);
+    }
+    printf("\n");
+  }
+}
+// Performs the dot product of on row and one column that are of the same length (size)
+// The row is taken from the specified row (row) in mat1 and the column comes from the
+// speci
+double mult_row_col(double** mat1, double** mat2, int row, int col, int size) {
+  int i;
+  double sum = 0;
+  for(i = 0; i < size; i++) {
+    sum += mat1[row][i] * mat2[i][col];
+  }
+  return sum;
+}
 
 double** create_array(int row, int col) {
   int i;
   double** matrix;
 
-  matrix = (double **) malloc(row * sizeof(*double));
+  matrix = (double **) malloc(row * sizeof(double*));
   for(i=0; i < col; i++) {
     matrix[i] = (double *) malloc(col * sizeof(double));
   }
@@ -45,17 +95,34 @@ int read_file(char *fileName, double** mat, int row, int col)
   }
   while ((line = fgets(buffer, sizeof(buffer), fstream)) != NULL)
   {
+    j = 0;
     record = strtok(line, ",");
     while (record != NULL)
     {
-      printf("record : %s", record); //here you can put the record into the array as per your requirement.
+      // printf("%s;", record); //here you can put the record into the array as per your requirement.
       mat[i][j++] = atof(record);
       record = strtok(NULL, ",");
+      // Check for column overflow
+      if (j > col) {
+        fprintf(stderr, "ERROR! Matrix %s does not have the same user supplied column dimensions!\n", fileName);
+        exit(0);
+      }
     }
+    // printf("\n");
     ++i;
+    // Check for row overflow
+    if (i > row) {
+      fprintf(stderr, "ERROR! Matrix %s does not have the same user supplied row dimensions!\n", fileName);
+      exit(0);
+    }
   }
-  printf("stuff");
-  return 1;
+  // check that file matrix size equals correct dimensions
+  if (i == row && j == col) {
+    return 1;
+  } else {
+    fprintf(stderr, "ERROR! Matrix %s does not have the same user supplied dimensions!\n", fileName);
+    exit(0);
+  }
   // i == number of rows, j == number of columns
 }
 
@@ -64,13 +131,24 @@ int main()
   // printf() displays the string inside quotation
   int dim_a_row, dim_a_col, dim_b_row, dim_b_col;
   char a = 'A', b = 'B';
-  float** matrix_a, matrix_b;
-  char* aFile = "A.csv";
+  double ** matrix_a, ** matrix_b, ** product;
+  char* aFile = "A.csv"; 
+  char* bFile = "B.csv";
+  int64_t sTime;
+
   read_dimension(&dim_a_row, &dim_a_col, a);
   matrix_a = create_array(dim_a_row, dim_a_col);
-  read_file(aFile, dim_a_row, dim_a_col);
+  read_file(aFile, matrix_a, dim_a_row, dim_a_col);
+
+  // print_matrix("C.csv", matrix_a, dim_a_row, dim_a_col);
 
   read_dimension(&dim_b_row, &dim_b_col, b);
-  // printf("Quantities are: %d, %d\n",dim_a_row, dim_a_col);
+  matrix_b = create_array(dim_b_row, dim_b_col);
+  read_file(bFile, matrix_b, dim_b_row, dim_b_col);
+  // print_matrix("C.csv", matrix_b, dim_b_row, dim_b_col);
+  sTime = utime_now();
+  product = dot_product(matrix_a, matrix_b, dim_a_row, dim_a_col, dim_b_row, dim_b_col);
+  printf("\nElapsed Time: %" PRId64 "\n", utime_now() - sTime);
+  // print_matrix("C.csv", product, dim_a_row, dim_b_col);
   return 0;
 }
